dd766db9713e9e55cb2e12ce230d6da081b41bea
DJVegaG <domingo.vega1901@alumnos.ubiobio.cl>
2023-12-04 18:47:43 -0300

    UPDATE 04-12 Conexion arduino

--- procesamientoDeImagenesv1.py
+++ procesamientoDeImagenesv1.py
@@ -4,10 +4,53 @@ from PIL import Image, ImageTk
 import imutils
 import cv2
 import os
+import serial.tools.list_ports
 
 # Variables globales a utilizar para la captura
 captura1 = None
 captura2 = None
+arduino = None
+
+# Funcion para obtener los puertos disponibles de arduino
+def obtener_puertos_disponibles():
+    puertos_disponibles = [
+        port.device for port in serial.tools.list_ports.comports()
+    ]
+    return puertos_disponibles
+
+# Funcion para conectar a arduino
+def conectar_arduino(numero_puerto, label_estado):
+    try:
+        arduino = serial.Serial(port=numero_puerto, baudrate=9600, timeout=1)
+        label_estado.config(
+            text=f"Conectado a Arduino en el puerto {numero_puerto}")
+        return arduino
+    except Exception as e:
+        arduino = None
+        label_estado.config(
+            text=f"No se pudo conectar a Arduino en el puerto {numero_puerto}")
+        return None
+
+# Funcion para desconectar arduino
+def desconectar_arduino(arduino, label_estado):
+    if arduino is not None:
+        arduino.close()
+        label_estado.config(text="Arduino desconectado")
+
+
+def conectar_desconectar_arduino():
+    puerto_seleccionado = combobox_arduino.get()
+    global arduino
+    if not puerto_seleccionado or puerto_seleccionado == "Selecciona un puerto":
+        label_estado.config(text="Selecciona un puerto")
+        return
+    if arduino is None:
+        arduino = conectar_arduino(puerto_seleccionado, label_estado)
+        if arduino:
+            boton_conectar_desconectar.config(text="Desconectar")
+    else:
+        desconectar_arduino(arduino, label_estado)
+        boton_conectar_desconectar.config(text="Conectar")
 
 # Función para obtener la lista de cámaras conectadas
 def obtener_camaras_disponibles():
@@ -37,10 +80,17 @@ def iniciar_captura(captura, label_imagen):
             captura.release()
 
 # Función para capturar y guardar la imagen
-def capturar_guardar(captura, carpeta_destino, nombre_archivo):
+def capturar_guardar_mostrar(captura, carpeta_destino, nombre_archivo, label_captura):
     if captura is not None:
         ret, frame = captura.read()
         if ret:
+            frame = imutils.resize(frame, width=311)
+            frame = imutils.resize(frame, height=241)
+            imagen_captura = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(imagen_captura)
+            img_tk = ImageTk.PhotoImage(image=img)
+            label_captura.configure(image=img_tk)
+            label_captura.image = img_tk
             ruta_completa = os.path.join(carpeta_destino, f"{nombre_archivo}.png")
             cv2.imwrite(ruta_completa, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             print(f"Captura guardada en: {ruta_completa}")
@@ -108,8 +158,9 @@ def cerrar_ventana():
 
 # Configuración de la ventana
 ventana = tk.Tk()
-ventana.geometry("750x800")
-ventana.resizable(0, 0)
+ancho_pantalla = ventana.winfo_screenwidth()
+alto_pantalla = ventana.winfo_screenheight()
+ventana.geometry(f"{int(ancho_pantalla * 0.8)}x{int(alto_pantalla * 0.8)}")
 ventana.title("Proyecto procesamiento de imagen con webcam")
 ventana.protocol("WM_DELETE_WINDOW", cerrar_ventana)
 
@@ -139,22 +190,22 @@ boton_conectar2.place(x=390, y=370, width=120, height=30)
 boton_desconectar2 = tk.Button(ventana, text="Desconectar", command=desconectar_camara_2, state=tk.DISABLED)
 boton_desconectar2.place(x=520, y=370, width=120, height=30)
 
-boton_capturar2 = tk.Button(ventana, text="Capturar", command=lambda: capturar_guardar(captura2, etiqueta_ruta.cget("text").split(": ")[1], entrada_nombre.get()))
-boton_capturar2.place(x=450, y=410, width=120, height=30)
+boton_capturar = tk.Button(ventana, text="Capturar", command=lambda: capturar_guardar_mostrar(captura2, etiqueta_ruta.cget("text").split(": ")[1], entrada_nombre.get(), label_captura))
+boton_capturar.place(x=820, y=370, width=120, height=30)
 
 # Etiqueta y botón para seleccionar la carpeta de destino
-etiqueta_ruta = tk.Label(ventana, text="Carpeta de destino:")
-etiqueta_ruta.place(x=50, y=500, width=600, height=20)
+etiqueta_ruta = tk.Label(ventana, text="Carpeta de destino:", wraplength=300, justify="left")
+etiqueta_ruta.place(x=200, y=460, width=300, height=40)
 
 boton_seleccionar = tk.Button(ventana, text="Seleccionar carpeta", command=seleccionar_destino)
 boton_seleccionar.place(x=50, y=460, width=150, height=30)
 
 # Entrada para el nombre del archivo
 etiqueta_nombre = tk.Label(ventana, text="Nombre del archivo:")
-etiqueta_nombre.place(x=50, y=650, width=150, height=20)
+etiqueta_nombre.place(x=50, y=500, width=150, height=20)
 
 entrada_nombre = tk.Entry(ventana)
-entrada_nombre.place(x=210, y=645, width=150, height=30)
+entrada_nombre.place(x=190, y=500, width=150, height=30)
 entrada_nombre.insert(0, "nombre_archivo")
 
 # Cuadros de Imagen grises
@@ -163,6 +214,27 @@ label_imagen1.place(x=50, y=50, width=300, height=250)
 
 label_imagen2 = tk.Label(ventana, background="gray")
 label_imagen2.place(x=390, y=50, width=300, height=250)
+
+# Cuadros de Imagen capturada
+label_captura = tk.Label(ventana, background="gray")
+label_captura.place(x=730, y=50, width=300, height=250)
+
+# Obtener puertos de Arduino disponibles
+puertos_arduino = obtener_puertos_disponibles()
+
+# Combobox y botón para conectar a Arduino
+combobox_arduino = ttk.Combobox(ventana, values=puertos_arduino)
+combobox_arduino.set("Selecciona un puerto")
+combobox_arduino.place(x=50, y=550, width=200, height=23)
+
+# Boton para conectar y desconectar Arduino
+boton_conectar_desconectar = tk.Button(ventana,text="Conectar")
+boton_conectar_desconectar.place(x=50, y=600, width=120, height=30)
+boton_conectar_desconectar.bind("<Button-1>", lambda event: conectar_desconectar_arduino())
+
+# Etiqueta para mostrar el estado de la conexión
+label_estado = tk.Label(ventana, text="", wraplength=200, justify="left")
+label_estado.place(x=190, y=600, width=200, height=30)
 
 # Iniciar la ventana
 ventana.mainloop()
